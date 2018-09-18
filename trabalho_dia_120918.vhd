0191778d1997536287f9f33663099fa7a4756fe2
CarryPorch <jorgemfrocha20@gmail.com>
2018-09-18 01:51:26 -0300

    pre-compile

--- trabalho_dia_120918.vhd
+++ trabalho_dia_120918.vhd
@@ -18,6 +18,8 @@ end entity trabalho_dia_120918;
 --inicio da arquitetura do projeto inteiro
 architecture c_trabalho_dia_120918 of trabalho_dia_120918 is
 
+--XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
 component gerador_clock_duplo is
 port (
 	signal clock_in : in bit;
@@ -26,6 +28,8 @@ port (
 );
 end component gerador_clock_duplo;
 
+--XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
 component gerador_pwm is
 port (
 signal clock_in : in bit;
@@ -33,24 +37,30 @@ signal pwm1, pwm2, pwm3 : out bit
 );
 end component gerador_pwm;
 
+--XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
 component maq_primos is
 port (
 	signal pwm1 : in bit;
 	signal control: in bit;
-	signal estado : out bit_vector (3 downto 0)
+	signal estado : out bit_vector (3 downto 0);
+	signal tipo : out bit_vector (1 downto 0)
 );
 end component maq_primos;
 
-component controlador_display is
+--XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
+component controlador_display_new is
 port (
-signal clock : in bit;
-signal num : in bit_vector(3 downto 0);
-signal clock1, clock2 : in bit;
-signal num7, num6, num5, num4, 
-num3, num2, num1, num0 : out bit_vector (3 downto 0);
-signal en0, en1, en2, en3, en4, en5, en6, en7 : out bit
+signal pwm2, clock_out1, clock_out2 : in bit;
+signal estado : in bit_vector(3 downto 0);
+signal tipo : in bit_vector(1 downto 0);
+signal num7, num6, num5, num4, num3, num2, num1, num0 : out bit_vector (3 downto 0);
+signal en7, en6, en5, en4, en3, en2, en1, en0 : out bit
 );
 end component controlador_display;
+
+--XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 
 component conv_7seg is
 port (
@@ -60,6 +70,8 @@ port (
 );
 end component conv_7seg;
 
+--XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
 -- variaveis intermediarias
 --signal clk : bit; --iniciadas no programa atual, portanto nao precisa de intermediaria (?)
 --signal control : bit; --iniciadas no programa atual, portanto nao precisa de intermediaria (?)
@@ -67,9 +79,8 @@ signal clock_out1 : bit; --uma das saidas do gerador de clock duplo
 signal clock_out2 : bit; --outra saida do gerador de clock duplo
 signal pwm1, pwm2, pwm3 : bit; --saidas do gerador de pwm 
 signal estado : bit_vector (3 downto 0); --numero que sai da maquina de primos
---signal saida : bit_vector (1 downto 0); --Nao existe
-signal num7, num6, num5, num4, num3, num2, 
-       num1, num0 : bit_vector (3 downto 0); --Numeros enviados para cada display, pelo controlador de displays
+signal tipo : bit_vector (1 downto 0) --tipo do numero que sai da maquina de primos
+signal num7, num6, num5, num4, num3, num2, num1, num0 : bit_vector (3 downto 0); --Numeros enviados para cada display, pelo controlador de displays
 signal en7, en6, en5, en4, en3, en2, en1, en0 : bit; -- Os clocks de cada display
 signal pisca1 : bit;
 signal pisca2 : bit;
@@ -82,12 +93,13 @@ xgerador_clock_duplo: gerador_clock_duplo port map (clk, clock_out1, clock_out2)
 --no gerador de clock duplo, entra o clock inicial da placa, e saem dois clocks utilizados para o sistema
 
 xgerador_pwm: gerador_pwm port map (clk, pwm1, pwm2, pwm3);
---no gerador de pwm, entra o clock inicial da placa, e saem 3 clocks gerados, cada um com atrasos diferentes
+--no gerador de pwm, entra o clock inicial da placa, e saem 3 clocks gerados, cada um com um atraso diferente
 
-xmaq_primos: maq_primos port map (pwm1, control, estado);
---na maquina de primos entra o clock do pwm, o controle que determina se sao primos ou nao, e sai o estado, que e o numero
+xmaq_primos: maq_primos port map (pwm1, control, estado, tipo);
+--na maquina de primos entra o clock do pwm, o controle que determina se sao primos ou nao
+--e sai o estado, que e o numero em si, e o seu tipo, que determina seu "pisca"
 
-xcontrolador_display: controlador_display port map (pwm2, estado, clock_out1, clock_out2,
+xcontrolador_display: controlador_display port map (pwm2, clock_out1, clock_out2, estado, tipo,
 	 num7, num6, num5, num4, num3, num2, num1, num0,
 	 en7, en6, en5, en4, en3, en2, en1, en0);
 --no controlador do display, entram o pwm2, que e o clock atrasado, o estado, que e o numero gerado pela maquina de primos e os
